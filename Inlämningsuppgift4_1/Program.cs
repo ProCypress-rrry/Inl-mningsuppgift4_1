5d1a653d028fb9fba6e828ac535919aff2501bc9
Victor Johansson <vijo07001@utb.vaxjo.se>
2024-12-11 13:09:25 +0100

    Add project files.

---
+++ Inlämningsuppgift4_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Inlämningsuppgift4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++) 
+                Console.WriteLine(i);
+            for (int  j = 200; j > 179; j--)
+                Console.WriteLine(j);
+            for (int  k = 1000;  k < 1450; k+=50)
+                Console.WriteLine(k);
+        }
+    }
+}
